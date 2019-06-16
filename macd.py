7df24137f0ac95428f39cb60ef3ea9e6e2f3a4a8
Jian He <hejian1639@sina.com>
2019-06-16 23:24:04 +0800

    reorder input data

--- macd.py
+++ macd.py
@@ -9,30 +9,43 @@ import time
 def getstartdate(code):
     """obtain establish date of stock"""
     df = tushare.get_stock_basics()
-    date = df.ix[code]['timeToMarket']  # 上市日期YYYYMMDD
+    date = df.iloc[code]['timeToMarket']  # 上市日期YYYYMMDD
     return date
 
 
 def get_EMA(df, N):
+    # print(df)
+    # df['ema'] = None
+    ema = [0] * len(df)
+
     for i in range(len(df)):
         if i == 0:
-            df.ix[i, 'ema'] = df.ix[i, 'close']
-        if i > 0:
-            df.ix[i, 'ema'] = (2 * df.ix[i, 'close'] + (N - 1) * df.ix[i - 1, 'ema']) / (N + 1)
-    ema = list(df['ema'])
+            ema[i] = df.iloc[i]['close']
+        else:
+            ema[i] = (2 * df.iloc[i]['close'] + (N - 1) * ema[i - 1]) / (N + 1)
+
     return ema
 
 
 def get_MACD(df, short=12, long=26, M=9):
     a = get_EMA(df, short)
     b = get_EMA(df, long)
-    df['diff'] = pd.Series(a) - pd.Series(b)
-    # print(df['diff'])
+    ema = pd.DataFrame()
+    ema['a'] = a
+    ema['b'] = b
+
+    df['diff'] = ema['a'] - ema['b']
+    # print(df)
+    dea = [0] * len(df)
+
     for i in range(len(df)):
         if i == 0:
-            df.ix[i, 'dea'] = df.ix[i, 'diff']
-        if i > 0:
-            df.ix[i, 'dea'] = (2 * df.ix[i, 'diff'] + (M - 1) * df.ix[i - 1, 'dea']) / (M + 1)
+            dea[i] = df.iloc[i]['diff']
+        else:
+            dea[i] = (2 * df.iloc[i]['diff'] + (M - 1) * dea[i - 1]) / (M + 1)
+
+    df['dea'] = dea
+
     df['macd'] = 2 * (df['diff'] - df['dea'])
     return df
 
@@ -60,30 +73,35 @@ if __name__ == '__main__':
     """
     stocks = tushare.get_stock_basics()
 
-    results = []
+    results = None
     # start date
-    start_date = '2019-05-01'
+    start_date = '2018-01-01'
 
     # end date
     end_date = '2019-06-14'
 
     count = 0
-    for index, row in stocks.iterrows():
+    for code, row in stocks.iterrows():
         print(row.name)
-        result = tushare.get_h_data(code=row.name, index=True, start=start_date, end=end_date)
+        result = tushare.get_h_data(code=code, index=True, start=start_date, end=end_date)
         if result.empty:
             continue
 
+        result = result.sort_index()
+        result['code'] = code
+        result = result.reset_index()
+        get_MACD(result, 12, 26, 9)
         print(result)
-        print(cal_macd_system(result, 12, 26, 9))
-        results.append(result)
+        results = pd.concat([results, result], ignore_index=True)
+
+        result.to_csv('stock.csv', mode='a', header=count == 0, index=False)
 
         count += 1
-        if count == 5:
+        if count == 3:
             break
-        time.sleep(20)
+        # time.sleep(20)
 
     print(results)
 
     # save data to csv
-    results.to_csv('stock.csv', mode='w')
+    # results.to_csv('stock.csv', mode='w')
